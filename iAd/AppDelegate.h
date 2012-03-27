ef52d2b035760b6996b2fd41fd3ccf3043dcf40e
ipodishima <marian.paul2@gmail.com>
2012-03-27 17:06:15 +0200

    Initial Commit

---
+++ iAd/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iAd
+//
+//  Created by Marian PAUL on 27/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iAd/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iAd
+//
+//  Created by Marian PAUL on 27/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
